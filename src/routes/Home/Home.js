ea3c6cc601c5c2e2183325b0ddb87ef1191d2ddc
buzz1675 <buzz1675@gmail.com>
2024-01-30 17:42:08 +0000

    make homepage initial names

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -11,107 +11,106 @@ const Home = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div></div>
-        <h2 class="header">
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            J
+      <div className="title_container">
+        <h3 id="hi-my-name">Hey There,</h3>
+
+        <div className="name_container">
+          <div className="name_topline">
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              I
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              '
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              M
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              &nbsp;
+            </p>
+
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              J
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              O
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              S
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              H
+            </p>
+
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              ,
+            </p>
+          </div>
+          <div className="name_bottomline">
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              A
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              &nbsp;
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              B
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              U
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              D
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              D
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              I
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              N
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              G
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              &nbsp;
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              D
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              E
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              V
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              E
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              L
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              O
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              P
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              E
+            </p>
+            <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
+              R
+            </p>
+          </div>
+          <p className="name_description">
+            I'm a totally self taught Full-Stack developer with a passion for
+            coding. Welcome to my portfolio!
           </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            O
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            S
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            H
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            &nbsp;
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            B
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            U
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            S
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            L
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            E
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            Y
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            ,
-          </p>
-          <br />
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            B
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            U
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            D
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            D
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            I
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            N
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            G
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            &nbsp;
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            D
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            E
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            V
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            E
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            L
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            O
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            P
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            E
-          </p>
-          <p class="pop" onMouseEnter={(e) => addRubberBandClass(e.target)}>
-            R
-          </p>
-        </h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+      </div>
     </div>
   );
 };
