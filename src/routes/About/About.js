157062d116df0ee9e2767a2feb3f07b7cf10002c
buzz1675 <buzz1675@gmail.com>
2024-02-06 16:52:35 +0000

    work on about us section

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -1,12 +1,41 @@
-import React from "react"
-import './About.css'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div id='about' className="about_container">
-      <p>Hi this is my life</p>
+    <div id="about" className="about_container">
+      <div className="about_content_container">
+        <div className="about_title">
+          <h2 className="about_h2_title">About Me</h2>
+          <p>Learn more about my journey so far</p>
+        </div>
+        <div className="about_text">
+          <p>
+            Back in early 2023 I decided to start learning how to code and found
+            myself completely intrigued by the complexity but beauty of it.
+            Starting off with freeCodeCamp before moving onto CodeCdademy's Full
+            Stack Engineer Course, I learned HTML, CSS, Javscript, SQL, Node.js
+            and other essential technologies for software development from
+            scratch.
+          </p>
+          <br />
+          <p>
+            Back in early 2023 I decided to start learning how to code and found
+            myself completely intrigued by the complexity but beauty of it.
+            Starting off with freeCodeCamp before moving onto CodeCdademy's Full
+            Stack Engineer Course, I learned HTML, CSS and Javscript from
+            scratch
+          </p>
+          <br />
+          <p>
+            When I am not coding or running my business, you can find me biking,
+            mountaineering, climbing, hanging out with friends, playing
+            pickleball or getting lost in the world of video games
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default About;
