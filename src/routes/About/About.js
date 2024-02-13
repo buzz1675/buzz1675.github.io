eeadc91c59926c1c5365ccc99fe236a9adc9867c
buzz1675 <buzz1675@gmail.com>
2024-02-13 16:01:10 +0000

    style about us page better

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -21,32 +21,51 @@ const About = () => {
 
         <div className="about_text">
           <Reveal>
-            <p>
-              Back in early 2023 I decided to start learning how to code and
-              found myself completely intrigued by the complexity but beauty of
-              it. Starting off with freeCodeCamp before moving onto
-              CodeCdademy's Full Stack Engineer Course, I learned HTML, CSS,
-              Javscript, SQL, Node.js and other essential technologies for
-              software development from scratch.
-            </p>
+            <motion.div
+              variants={textVariant(0.45)}
+              initial="hidden"
+              whileInView="show"
+            >
+              <p>
+                Back in early 2023 I decided to start learning how to code and
+                found myself completely intrigued by the complexity but beauty
+                of it. Starting off with freeCodeCamp before moving onto
+                CodeCdademy's Full Stack Engineer Course, I learned HTML, CSS,
+                Javscript, SQL, Node.js and other essential technologies for
+                software development from scratch.
+              </p>
+            </motion.div>
           </Reveal>
           <br />
           <Reveal>
-            <p>
-              Back in early 2023 I decided to start learning how to code and
-              found myself completely intrigued by the complexity but beauty of
-              it. Starting off with freeCodeCamp before moving onto
-              CodeCdademy's Full Stack Engineer Course, I learned HTML, CSS and
-              Javscript from scratch
-            </p>
+            <motion.div
+              variants={textVariant(0.65)}
+              initial="hidden"
+              whileInView="show"
+            >
+              <p>
+                Back in early 2023 I decided to start learning how to code and
+                found myself completely intrigued by the complexity but beauty
+                of it. Starting off with freeCodeCamp before moving onto
+                CodeCdademy's Full Stack Engineer Course, I learned HTML, CSS
+                and Javscript from scratch
+              </p>
+            </motion.div>
           </Reveal>
           <br />
           <Reveal>
-            <p>
-              When I am not coding or running my business, you can find me
-              biking, mountaineering, climbing, hanging out with friends,
-              playing pickleball or getting lost in the world of video games
-            </p>
+            <motion.div
+              variants={textVariant(0.85)}
+              initial="hidden"
+              whileInView="show"
+            >
+              <p>
+                I have many hobbies that I enjoy outside of work. When I am not
+                coding or running my business, you can find me biking,
+                mountaineering, climbing, hanging out with friends, playing
+                pickleball or getting lost in the world of video games
+              </p>
+            </motion.div>
           </Reveal>
         </div>
       </div>
