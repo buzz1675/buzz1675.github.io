36580f1feefed1d62d1e70c0b756d321a2a0e66e
buzz1675 <buzz1675@gmail.com>
2024-02-13 14:19:42 +0000

    re animate skills section

--- src/routes/Skills/Skills.js
+++ src/routes/Skills/Skills.js
@@ -17,6 +17,7 @@ import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 import { RevealSkillIcon } from "../../components/Reveals/SkillsReveal";
 import { Reveal } from "../../components/Reveals/Reveal";
+import { fadeIn, textVariant } from "../../utils/motion";
 
 const Skills = () => {
   const skillsVariants = {
@@ -26,95 +27,136 @@ const Skills = () => {
   return (
     <div id="skills" className="skills_container">
       <div className="skills_content_container">
-        <div className="skills_title">
+        <motion.div
+          variants={textVariant(0.25)}
+          initial="hidden"
+          whileInView="show"
+          className="skills_title"
+        >
           <h2>My Skills</h2>
 
           <p>
             I have worked with a wide variety of technologies in web and
             software development. From Back-End to Front-End
           </p>
-        </div>
+        </motion.div>
         <div className="technologies_list">
           <div className="frontend_technologies">
-            <Reveal>
+            <motion.div
+              variants={textVariant(0.45)}
+              initial="hidden"
+              whileInView="show"
+            >
+              {" "}
               <h3>Front-End Technologies</h3>
-            </Reveal>
+            </motion.div>
+
             <div className="skill_icon_container">
-              <RevealSkillIcon delay={0.6}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon css" icon={faCss3} />
-                  </a>
-                  <p className="icon_name">CSS</p>
-                </div>{" "}
-              </RevealSkillIcon>
-              <RevealSkillIcon delay={0.7}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon html" icon={faHtml5} />
-                  </a>
-                  <p className="icon_name">HTML</p>
-                </div>{" "}
-              </RevealSkillIcon>
-              <RevealSkillIcon delay={0.8}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon js" icon={faJs} />
-                  </a>
-                  <p className="icon_name">JavaScript</p>
-                </div>{" "}
-              </RevealSkillIcon>
-              <RevealSkillIcon delay={0.9}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon react" icon={faReact} />
-                  </a>
-                  <p className="icon_name">React</p>
-                </div>
-              </RevealSkillIcon>
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 1, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon css" icon={faCss3} />
+                </a>
+                <p className="icon_name">CSS</p>
+              </motion.div>{" "}
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 2, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon html" icon={faHtml5} />
+                </a>
+                <p className="icon_name">HTML</p>
+              </motion.div>{" "}
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 3, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon js" icon={faJs} />
+                </a>
+                <p className="icon_name">JavaScript</p>
+              </motion.div>{" "}
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 4, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon react" icon={faReact} />
+                </a>
+                <p className="icon_name">React</p>
+              </motion.div>
             </div>
           </div>
           <div className="backend_technologies">
-            <Reveal>
+            <motion.div
+              variants={textVariant(0.45)}
+              initial="hidden"
+              whileInView="show"
+            >
+              {" "}
               <h3>Back-End Technologies</h3>
-            </Reveal>
-            <div className="skill_icon_container">
-              <RevealSkillIcon delay={0.6}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon node" icon={faNodeJs} />
-                  </a>
-                  <p className="icon_name">Node.js</p>
-                </div>
-              </RevealSkillIcon>
-              <RevealSkillIcon delay={0.7}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon github" icon={faGitAlt} />
-                  </a>
-                  <p className="icon_name">Git</p>
-                </div>
-              </RevealSkillIcon>{" "}
-              <RevealSkillIcon delay={0.8}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon
-                      className="icon database"
-                      icon={faDatabase}
-                    />
-                  </a>
-                  <p className="icon_name">SQL</p>
-                </div>
-              </RevealSkillIcon>{" "}
-              <RevealSkillIcon delay={0.9}>
-                <div className="skill_container">
-                  <a className="skills_icon">
-                    <FontAwesomeIcon className="icon stripe" icon={faStripe} />
-                  </a>
-                  <p className="icon_name">Stripe Integration</p>
-                </div>
-              </RevealSkillIcon>
-            </div>
+            </motion.div>
+
+            <motion.div
+              initial="hidden"
+              whileInView={"show"}
+              variants={fadeIn("right", "spring", 0.35 * 5, 0.75)}
+              className="skill_icon_container"
+            >
+              <div className="skill_container">
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon node" icon={faNodeJs} />
+                </a>
+                <p className="icon_name">Node.js</p>
+              </div>
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 6, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon github" icon={faGitAlt} />
+                </a>
+                <p className="icon_name">Git</p>
+              </motion.div>
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 7, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon
+                    className="icon database"
+                    icon={faDatabase}
+                  />
+                </a>
+                <p className="icon_name">SQL</p>
+              </motion.div>
+              <motion.div
+                initial="hidden"
+                whileInView={"show"}
+                variants={fadeIn("right", "spring", 0.35 * 8, 0.75)}
+                className="skill_container"
+              >
+                <a className="skills_icon">
+                  <FontAwesomeIcon className="icon stripe" icon={faStripe} />
+                </a>
+                <p className="icon_name">Stripe Integration</p>
+              </motion.div>
+            </motion.div>
           </div>
         </div>
       </div>
