5edfda92e945404d743bef94d14dd566936db2e6
buzz1675 <buzz1675@gmail.com>
2024-02-13 15:43:53 +0000

    style about title animation

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./About.css";
 import { Reveal } from "../../components/Reveals/Reveal";
+import { motion } from "framer-motion";
+import { textVariant } from "../../utils/motion";
 
 const About = () => {
   return (
-    
     <div id="about" className="about_container">
       <div className="about_content_container">
-        
-          <div className="about_title">
-            <h2 className="about_h2_title">About Me</h2>
-            <p>Learn more about my journey</p>
-          </div>
-        
+        <motion.div
+          variants={textVariant(0.25)}
+          initial="hidden"
+          whileInView="show"
+          className="about_title"
+        >
+          <h2 className="about_h2_title">About Me</h2>
+
+          <p>Learn more about my journey</p>
+        </motion.div>
+
         <div className="about_text">
           <Reveal>
             <p>
@@ -26,26 +32,25 @@ const About = () => {
           </Reveal>
           <br />
           <Reveal>
-          <p>
-            Back in early 2023 I decided to start learning how to code and found
-            myself completely intrigued by the complexity but beauty of it.
-            Starting off with freeCodeCamp before moving onto CodeCdademy's Full
-            Stack Engineer Course, I learned HTML, CSS and Javscript from
-            scratch
-          </p>
+            <p>
+              Back in early 2023 I decided to start learning how to code and
+              found myself completely intrigued by the complexity but beauty of
+              it. Starting off with freeCodeCamp before moving onto
+              CodeCdademy's Full Stack Engineer Course, I learned HTML, CSS and
+              Javscript from scratch
+            </p>
           </Reveal>
           <br />
           <Reveal>
-          <p>
-            When I am not coding or running my business, you can find me biking,
-            mountaineering, climbing, hanging out with friends, playing
-            pickleball or getting lost in the world of video games
-          </p>
+            <p>
+              When I am not coding or running my business, you can find me
+              biking, mountaineering, climbing, hanging out with friends,
+              playing pickleball or getting lost in the world of video games
+            </p>
           </Reveal>
         </div>
       </div>
     </div>
-    
   );
 };
 
