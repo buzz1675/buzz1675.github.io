9f051f683f0c7dfafd16a6d3524ad332745252ea
buzz1675 <buzz1675@gmail.com>
2024-02-15 15:04:40 +0000

    make open projectcard component

--- src/components/OpenProjectCard/OpenProjectCard.js
+++ src/components/OpenProjectCard/OpenProjectCard.js
@@ -1,11 +1,32 @@
 import React from "react"
+import { motion } from "framer-motion";
 
-const name = (props) => {
+const OpenProjectCard = (project) => {
   return (
-    <div>
-      
-    </div>
+    <motion.div
+                        layoutId={`card-container`}
+                        key={project.name}
+                        initial={{ scale: 0.8, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        exit={{ scale: 0.8, opacity: 0 }}
+                      >
+                        <motion.div className="relative">
+                          <motion.button onClick={() => setSelectedProject("")}>
+                            Close
+                          </motion.button>
+                          <motion.h2>{project.title}</motion.h2>
+                          <motion.h5>{project.title}</motion.h5>
+                          <motion.p>{project.description}</motion.p>
+                          <motion.p
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                          >
+                            Additional content can go here!
+                          </motion.p>
+                        </motion.div>
+                      </motion.div>
   )
 };
 
-export default name;
+export default OpenProjectCard;
