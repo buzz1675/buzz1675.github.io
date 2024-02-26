92f14097a141c7e4f1bd7028f38e88b826439f17
buzz1675 <buzz1675@gmail.com>
2024-02-26 14:23:33 +0000

    change data structure for projects

--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -15,6 +15,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import OpenProjectCard from "../../components/OpenProjectCard/OpenProjectCard";
 import SpotifyLogo from "../../imgs/Screenshot 2024-02-21 141936.png";
 import SpotifyAppScreenshot from "../../imgs/SpotifyAppScreenshot.png";
+import {projects} from "../../utils/data.js";
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -23,93 +24,7 @@ const Projects = () => {
     console.log(selectedProject);
   }, [selectedProject]);
 
-  const projects = [
-    {
-      title: "Reddit Clone",
-      ThumbnailImage: SpotifyLogo,
-      screenshot: SpotifyAppScreenshot,
-
-      description:
-        "A clone of Reddit, a social media platform where users can post, comment, and upvote posts.",
-      technologies: [
-        { name: "JavaScript", icon: faJs },
-        { name: "HTML", icon: faHtml5 },
-        { name: "CSS", icon: faCss3 },
-        { name: "React", icon: faReact },
-        { name: "Node.js", icon: faNodeJs },
-        { name: "Git", icon: faGit },
-      ],
-    },
-    {
-      title: "Spotify Clone",
-      ThumbnailImage: SpotifyLogo,
-      screenshot: SpotifyAppScreenshot,
-
-      shortDescription:
-        "This is an app that allows users to search for songs and add them to custom playlists to save.",
-
-      description:
-        "A clone of Spotify, a music streaming platform where users can search for songs and add them to custom playlists to save.",
-      technologies: [
-        { name: "JavaScript", icon: faJs },
-        { name: "HTML", icon: faHtml5 },
-        { name: "CSS", icon: faCss3 },
-        { name: "React", icon: faReact },
-        { name: "Node.js", icon: faNodeJs },
-        { name: "Git", icon: faGit },
-      ],
-    },
-    {
-      title: "eCommerce Site",
-      ThumbnailImage: SpotifyLogo,
-      screenshot: SpotifyAppScreenshot,
-
-      description:
-        "Final CodeCademy Project of an eCommerce site with Stripe integration, backend development, and frontend design.",
-      technologies: [
-        { name: "JavaScript", icon: faJs },
-        { name: "HTML", icon: faHtml5 },
-        { name: "CSS", icon: faCss3 },
-        { name: "React", icon: faReact },
-        { name: "Node.js", icon: faNodeJs },
-        { name: "Git", icon: faGit },
-        { name: "Stripe", icon: faStripe },
-        { name: "SQL", icon: faDatabase },
-      ],
-    },
-    {
-      title: "Reddit Clone",
-      ThumbnailImage: SpotifyLogo,
-      screenshot: SpotifyAppScreenshot,
-
-      description:
-        "A clone of Reddit, a social media platform where users can post, comment, and upvote posts.",
-      technologies: [
-        { name: "JavaScript", icon: faJs },
-        { name: "HTML", icon: faHtml5 },
-        { name: "CSS", icon: faCss3 },
-        { name: "React", icon: faReact },
-        { name: "Node.js", icon: faNodeJs },
-        { name: "Git", icon: faGit },
-      ],
-    },
-    {
-      title: "Reddit Clone",
-      ThumbnailImage: SpotifyLogo,
-      screenshot: SpotifyAppScreenshot,
-
-      description:
-        "A clone of Reddit, a social media platform where users can post, comment, and upvote posts.",
-      technologies: [
-        { name: "JavaScript", icon: faJs },
-        { name: "HTML", icon: faHtml5 },
-        { name: "CSS", icon: faCss3 },
-        { name: "React", icon: faReact },
-        { name: "Node.js", icon: faNodeJs },
-        { name: "Git", icon: faGit },
-      ],
-    },
-  ];
+  
 
   return (
     <div id="projects" className="projects_container">
