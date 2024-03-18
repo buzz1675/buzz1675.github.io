eb5de7ad59e00ca478fc2d81e1b6aa505ef7446b
Josh Busley <buzz1675@gmail.com>
2024-03-18 15:08:58 +0000

    add extra info to about us

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -44,11 +44,12 @@ const About = () => {
               whileInView="show"
             >
               <p>
-                Back in early 2023 I decided to start learning how to code and
-                found myself completely intrigued by the complexity but beauty
-                of it. Starting off with freeCodeCamp before moving onto
-                CodeCdademy's Full Stack Engineer Course, I learned HTML, CSS
-                and Javscript from scratch
+                I have used the skills and knowledge I have acquired in the past
+                12 months to build various different projects, including this
+                one, to test and practise my skills and abilities. Not only have I made projects for fun, but I have also used
+                these in great effect in a practical, real world scenarios to aid my business. Using the
+                techniques and strategies I have learned I have constructed and developed several components to further aid in
+                my business' development.
               </p>
             </motion.div>
           </Reveal>
@@ -63,7 +64,7 @@ const About = () => {
                 I have many hobbies that I enjoy outside of work. When I am not
                 coding or running my business, you can find me biking,
                 mountaineering, climbing, hanging out with friends, playing
-                pickleball or getting lost in the world of video games
+                pickleball or getting lost in the world of video games.
               </p>
             </motion.div>
           </Reveal>
