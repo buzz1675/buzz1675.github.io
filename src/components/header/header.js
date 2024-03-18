10a26c58a5826ce3bb82bee914b79ae6bce71068
Josh Busley <buzz1675@gmail.com>
2024-03-18 14:21:53 +0000

    make header disappear on scroll

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LetterJ from "../../imgs/Letter J.png";
 import "./header.css";
 import { Link } from "react-scroll";
+
 const Header = () => {
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+
+      if (scrollTop === 0) {
+        setIsTop(true);
+      } else {
+        setIsTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="header_container">
+    <div className={`header_container ${isTop ? 'show' : 'hide'}`}>
       <img src={LetterJ} alt="Letter J" className="LetterJ" />
       <div className="nav_menu">
         <ul>
           <li className="nav_link">
-          <Link
+            <Link
               activeClass="active"
               to="home"
               spy={true}
@@ -45,7 +66,7 @@ const Header = () => {
             </Link>
           </li>
           <li className="nav_link">
-          <Link
+            <Link
               activeClass="active"
               to="skills"
               spy={true}
@@ -57,7 +78,7 @@ const Header = () => {
             </Link>
           </li>
           <li className="nav_link">
-          <Link
+            <Link
               activeClass="active"
               to="projects"
               spy={true}
